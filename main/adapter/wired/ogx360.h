0acbc051df81b9154c5ee2331b0a8d893408a1aa
Jack Rayner <jack.rayner1997@gmail.com>
2024-01-10 00:12:36 +0000

    Rename ogx360_from_generic to ogx360_from_wired_ctrl
    Update rumble

--- main/adapter/wired/ogx360.h
+++ main/adapter/wired/ogx360.h
@@ -3,6 +3,6 @@
 #include "adapter/config.h"
 
 void ogx360_meta_init(struct wired_ctrl *ctrl_data);
-void ogx360_from_generic(int32_t dev_mode, struct wired_ctrl *ctrl_data, struct wired_data *wired_data);
+void ogx360_from_wired_ctrl(int32_t dev_mode, struct wired_ctrl *ctrl_data, struct wired_data *wired_data);
 
 #endif /* _OGX360_H_ */
